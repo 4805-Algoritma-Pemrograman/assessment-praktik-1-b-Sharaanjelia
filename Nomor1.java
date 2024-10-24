f7ff2ccdd9543e72f3338ea0253704912198be66
Sharaanjelia <sharaanjelia236@gmail.com>
2024-10-24 09:32:02 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,7 +1,34 @@
-
+improt.java.util.scanner;
 
 public class Nomor1{
     public static void main(String[] args){
-      
+     Scanner snaner = new Scanner(system.int)
+                                  
+          //input jumalah nilai jualan
+         system.out.print( menghitung jumlah total penjualan sales)
+         double jumlah total penjualan = Scanner.nextdouble;
+    
+                                  
+         //variabel menyimpan komisi
+         int.double; double komisi:
+
+         //hitung jumlah komisi
+          if (totalPenjualan <= 500000) {
+              // 10% untuk penjualan <= 500.000
+            komisi = totalPenjualansales * 0.10; // 10% untuk penjualan <= 500.000
+        } else {
+              // 10% untuk 500.000 pertama
+            double komisiPertama = 500000 * 0.10; // 10% untuk 500.000 pertama
+            double sisaPenjualan = totalPenjualan - 500000;
+              // 15% untuk sisa penjualan
+            double komisiSisa = sisaPenjualan * 0.15; // 15% untuk sisa penjualan
+              // jumlah total komisi
+            komisi = komisiPertama + komisiSisa ; // jumlah total komisi
+             
+             // Output hasil
+        System.out.printf("Total penjualan sales:sisa stok, totalPenjualan);
+
+        scanner.close()
+        }        
     }
 }

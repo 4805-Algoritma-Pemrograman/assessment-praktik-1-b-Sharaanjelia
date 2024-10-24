737049c9c9c7c10887e3f18dbdec020a4e3a5123
Sharaanjelia <sharaanjelia236@gmail.com>
2024-10-24 10:04:54 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,33 +2,30 @@ improt.java.util.scanner;
 
 public class Nomor1{
     public static void main(String[] args){
-     Scanner snaner = new Scanner(system.int)
-                                  
-          //input jumalah nilai jualan
-         system.out.print( menghitung jumlah total penjualan sales)
-         double jumlah total penjualan = Scanner.nextdouble;
-    
-                                  
-         //variabel menyimpan komisi
-         int.double; double komisi:
+     Scanner scnaner = new Scanner(system.int)
 
-         //hitung jumlah komisi
-          if (totalPenjualan <= 500000) {
-              // 10% untuk penjualan <= 500.000
-            komisi = totalPenjualansales * 0.10; // 10% untuk penjualan <= 500.000
+        // Input nilai penjualan
+        System.out.print("Masukkan jumlah total  penjualan salesman: ");
+        double totalPenjualan = scanner.nextDouble();
+
+        // Variabel untuk menyimpan komisi
+        double komisi; double total pembelian;
+
+        // Hitung komisi berdasarkan ketentuan
+        if (totalPenjualan <= 500000) {
+            komisi = totalPenjualan * 0.10; // 10% untuk penjualan <= 500.000
         } else {
-              // 10% untuk 500.000 pertama
             double komisiPertama = 500000 * 0.10; // 10% untuk 500.000 pertama
-            double sisaPenjualan = totalPenjualan - 500000;
-              // 15% untuk sisa penjualan
+            double sisaPenjualansales = totalPenjualan - 500000;
             double komisiSisa = sisaPenjualan * 0.15; // 15% untuk sisa penjualan
-              // jumlah total komisi
-            komisi = komisiPertama + komisiSisa ; // jumlah total komisi
-             
-             // Output hasil
-        System.out.printf("Total penjualan sales:sisa stok, totalPenjualan);
+            komisi = komisiPertama + komisiSisa; // Total komisi
+        }
 
-        scanner.close()
-        }        
+        // Output hasil
+        System.out.printf("Total penjualan salesman: %.0f\n", totalPenjualan);
+        System.out.printf("Komisi yang didapatkan = %.1f\n", komisi);
+
+        scanner.close();
     }
 }
+
